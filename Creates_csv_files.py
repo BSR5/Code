bdb31647dd6046b159c7f69dc7770281d77c1e3a
Ben <Benmartinez@live.co.uk>
2022-12-13 23:36:54 +0000

    Removed inline comments

--- Creates_csv_files.py
+++ Creates_csv_files.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-insurance_data = pd.read_csv("insurance.csv") # Saves the insurance.csv file
+insurance_data = pd.read_csv("insurance.csv")
 
-non_smokers_data = insurance_data.loc[insurance_data["smoker"] == "no"] # Saves all data for the smokers column
-smokers_data = insurance_data.loc[insurance_data["smoker"] == "yes"] # Saves all data for the smokers column
+non_smokers_data = insurance_data.loc[insurance_data["smoker"] == "no"]
+smokers_data = insurance_data.loc[insurance_data["smoker"] == "yes"]
 
-non_smokers_data.to_csv("non_smokers_data.csv") # Writes the smokers_data to the new csv file
-smokers_data.to_csv("smokers_data.csv") # Writes the smokers_data to the new csv file
+non_smokers_data.to_csv("non_smokers_data.csv")
+smokers_data.to_csv("smokers_data.csv")
--- Extra_cost.py
+++ Extra_cost.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
-# Gathers data from the "charges" column in the later defined csv file
+
 def column_data(file_name, column_name):
-    data = pd.read_csv(file_name) # Reads the DataFrame into the "data" variable
-    column = data[column_name]  # Gets the data from the "Charges" column and saves it to the "column variable"
+    data = pd.read_csv(file_name)
+    column = data[column_name]
     return column
 
-# Sortes the data in a specified column into reverse order
 def sort_data_in_column(column1, column2):
-    zipped_column = list(zip(column1)) # Turns the tupled data into a single list
-    sorted_column = sorted(zipped_column, reverse = True) # Sorts the column into reverse order
-    column2_counted = len(column2) # Counts the length of how many items in column 2
-    top_num_column = sorted_column[:column2_counted] #selects the top results matching column2
-    return [value for value, *_ in top_num_column] # Extracts the values from the tuples in the "top_num_column1" list and returns it to the call
+    zipped_column = list(zip(column1))
+    sorted_column = sorted(zipped_column, reverse = True)
+    column2_counted = len(column2)
+    top_num_column = sorted_column[:column2_counted]
+    return [value for value, *_ in top_num_column]
 
-# Adds the defined columns together
 def sum_of_column(column, column2):
-    column = [float(x) for x in column] # Convert the strings in the column to numbers
-    column2 = [float(x) for x in column2] # Convert the strings in the column to numbers
-    sum_column1 = sum(column) # Adds all the data in the first specified column
-    sum_column2 = sum(column2) # Adds all the data in the second specified column
-    result =  sum_column1 - sum_column2 # Subtracts both totals
+    column = [float(x) for x in column]
+    column2 = [float(x) for x in column2]
+    sum_column1 = sum(column)
+    sum_column2 = sum(column2)
+    result =  sum_column1 - sum_column2
     return result
